d67f5f09b850c83f0792fd9ed612c6ec4a2b59bb
Dulip Sameeera <dsgitrepo101@gmail.com>
2025-10-19 05:56:55 +0530

    add changes to vs settings

--- obj/Debug/net8.0-windows/eshift.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/eshift.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eshift")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f7f250f0b4eeca9975e4167036a72b664c9908")]
 [assembly: System.Reflection.AssemblyProductAttribute("eshift")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eshift")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
